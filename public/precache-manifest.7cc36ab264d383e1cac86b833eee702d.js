10d4946107b16fa76515ee82dfcb8720adbab2ce
joniqfzhang <qfzhang18@hotmail.com>
2020-07-25 17:53:03 +1000

    Finished version to deploy onto Heroku

---
+++ public/precache-manifest.7cc36ab264d383e1cac86b833eee702d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e32c3081de01cbaa3f2e4fc28bc89a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67d0a4b48e7762c5f2af",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6ed20c7e815dbb470666",
+    "url": "/static/js/2.86e108d7.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.86e108d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d0a4b48e7762c5f2af",
+    "url": "/static/js/main.2dafecaa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "71f6e6b56bac901324d3ce5770c9cb24",
+    "url": "/static/media/logo.71f6e6b5.PNG"
+  }
+]);
